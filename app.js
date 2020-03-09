0e1d136588f205640b66f3ed92c5b3f256067520
Sahal Tariq <sahaltariq@pk.folio3.com>
2020-03-09 17:49:29 +0500

    node `

--- app.js
+++ app.js
@@ -1,14 +1,79 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+let jwt = require('jsonwebtoken');
+let config = require('./config');
+let middleware = require('./middleware');
+var path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+class HandlerGenerator {
+  login (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    // For the given username fetch user from DB
+    let mockedUsername = 'admin';
+    let mockedPassword = 'password';
+
+    if (username && password) {
+      if (username === mockedUsername && password === mockedPassword) {
+        let token = jwt.sign({username: username},
+          config.secret,
+          { expiresIn: '24h' // expires in 24 hours
+          }
+        );
+        // return the JWT token for the future API calls
+        res.json({
+          success: true,
+          message: 'Authentication successful!',
+          token: token
+        });
+      } else {
+        res.send(403).json({
+          success: false,
+          message: 'Incorrect username or password'
+        });
+      }
+    } else {
+      res.send(400).json({
+        success: false,
+        message: 'Authentication failed! Please check the request'
+      });
+    }
+  }
+  index (req, res) {
+    res.json({
+      success: true,
+      message: 'Index page'
+    });
+  }
+}
+
+// Starting point of the server
+function main () {
+  let app = express(); // Export app for other routes to use
+  let handlers = new HandlerGenerator();
+  const port = process.env.PORT || 8000;
+  app.use(bodyParser.urlencoded({ // Middleware
+    extended: true
+  }));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, 'public')));
+  // Routes & Handlers
+  app.post('/api/login', handlers.login);
+  app.get('/login',(req,res)=>{res.render("login",function(err,html){
+    if(err) {
+        res.send(404).json({
+            success: false,
+            message: 'Page not found'
+          });
+    }
+    res.send(html);
+  })});
+  app.get('/', middleware.checkToken, handlers.index);
+  app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+}
+
+main();
